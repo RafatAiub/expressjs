de04e5a98abf1c52a622cf22572ba989f2f5e1e4
rafat <rafat@eatlbd.com>
2024-09-10 18:15:54 +0600

    passing argument into Middleware we use Wrapper function and also handle error

--- hello/middleWare.js
+++ hello/middleWare.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+
+const loggerWrapper = (options) => {
+  return (req, res, next) => {
+    if (options.log) {
+      console.log(
+        `${new Date(Date.now()).toLocaleString()} - ${req.method} - ${
+          req.originalUrl
+        }- ${req.protocol}- ${req.ip}`
+      );
+    }
+    throw new Error("oh!!!!! no error occure in this logger Middleware");
+  };
+};
+
 const logger = (req, res, next) => {
   console.log(
     `${new Date(Date.now()).toLocaleString()} - ${req.method} - ${
@@ -8,6 +22,7 @@ const logger = (req, res, next) => {
     }- ${req.protocol}- ${req.ip}`
   );
   next();
+  //   throw new Error("oh!!!!! no error occure in this logger Middleware");
 };
 
 app.get("/", (req, res) => {
@@ -26,7 +41,8 @@ app.get("/application-level", (req, res) => {
 
 const adminRoute = express.Router();
 
-adminRoute.use(logger); // use logger middleware only in AdminPortal
+// adminRoute.use(logger); // use logger middleware only in AdminPortal
+adminRoute.use(loggerWrapper({ log: false }));
 
 app.use("/admin", adminRoute, (req, res) => {
   res.send("Admin Page ................");
@@ -35,6 +51,16 @@ app.use("/admin", adminRoute, (req, res) => {
 adminRoute.get("/dashboard", (req, res) => {
   res.send("welcome to admin dashboard");
 });
+
+// to handle error create another middleware
+
+const handleError = (err, req, res, next) => {
+  // must be use the 4 parameter
+  console.log("the error message:", err.message);
+  res.status(500).send("Server Side Error!!");
+};
+
+adminRoute.use(handleError);
 
 app.listen(5555, () => {
   console.log("listening on port 5555");
